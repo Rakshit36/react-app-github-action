4c87508c631a3d247660457e7d228d27a50ecb68
Rakshit Kamboj <rakshitkamboj@Rakshits-MacBook-Air.local>
2025-05-02 16:51:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>My React App</h1>
+      <h2>my react Implemenetation with CI/CD</h2>
+      <h3>With Github Action</h3>
+    </div>
+  )
+}
+
+export default App
